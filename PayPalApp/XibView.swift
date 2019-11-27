9bee0604098a364a277338470130e814f9a27416
mohamedosama2074 <iosengks74@gmail.com>
2019-11-27 13:03:12 +0200

    Initial Commit

---
+++ PayPalApp/XibView.swift
@@ -0,0 +1,16 @@
+//
+//  XibView.swift
+//  PayPalApp
+//
+//  Created by Mohamed on 11/27/19.
+//  Copyright © 2019 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import UIKit
+
+class XibView: UIView {
+
+   
+    
+
+}
